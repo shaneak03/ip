625f57d09e47a6b877ac09a538916cc7bd65c1f9
shaneak03 <shaneak03@gmail.com>
2025-08-16 21:56:05 +0800

    Update chatbot name to Barry

---
+++ src/main/java/Barry.java
@@ -0,0 +1,14 @@
+public class Barry {
+    public static void main(String[] args) {
+        String logo = "__________    _____ _________________________.___.\n"
+                      + "\\______   \\  /  _  \\\\______   \\______   \\__  |   |\n"
+                      + " |    |  _/ /  /_\\  \\|       _/|       _//   |   |\n"
+                      + " |    |   \\/    |    \\    |   \\|    |   \\\\____   |\n"
+                      + " |______  /\\____|__  /____|_  /|____|_  // ______|\n"
+                      + "        \\/         \\/       \\/        \\/ \\/    \n";
+        String line = "____________________________________________________________\n";
+        System.out.println("Hello I'm Barry\n" + logo + "What can I do for you?\n" + line);
+
+        System.out.println("Goodbye! Hope to see you again soon!\n" + line);
+    }
+}
