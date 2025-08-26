d9ddf87933d709fb7ebe6b2af7af3c4c8dd24937
shaneak03 <shaneak03@gmail.com>
2025-08-26 11:44:27 +0800

    Add datetime formatting

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,19 +1,28 @@
-public class DeadlineTask extends Task {
-    private String deadline;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public DeadlineTask(String desc, String deadline) {
+public class DeadlineTask extends Task {
+    private LocalDateTime deadline;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
+
+    public DeadlineTask(String desc, String deadlineStr) {
+        super(desc);
+        this.deadline = LocalDateTime.parse(deadlineStr, INPUT_FORMAT);
+    }
+
+    public DeadlineTask(String desc, LocalDateTime deadline) {
         super(desc);
         this.deadline = deadline;
     }
 
     @Override
     public String toSaveString() {
-        return "d" + super.toSaveString() +
-                String.format("| %s ", this.deadline);
+        return "d" + super.toSaveString() + String.format("| %s ", deadline.format(INPUT_FORMAT));
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), deadline);
+        return String.format("[D]%s (by: %s)", super.toString(), deadline.format(OUTPUT_FORMAT));
     }
 }
